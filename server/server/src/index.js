72fe35e07c99279fd7e754d07e2c97cb4d7594bf
P M Manmohan <manu6520042@gmail.com>
2024-07-03 20:59:38 +0530

    mover server.js

--- server/server/src/index.js
+++ server/server/src/index.js
@@ -1,5 +1,5 @@
 import express from "express"
-import pool from "../db.js"
+import pool from "../../db.js"
 import cors from "cors"
 import { v4 as uuidv4 } from "uuid";
 import bcrypt from 'bcrypt';
